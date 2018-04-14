1394f65369cc71213389d00614e3a96f60ffe522
David Sabalete <dsabalete@gmail.com>
2018-04-14 17:40:25 +0200

    Add Book Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 
 
 // components
-import BookList from './components/BookList';
+import BookList from './components/BookList'
+import AddBook from './components/AddBook'
 
 // apollo client setup
 const client = new ApolloClient({
@@ -18,6 +19,7 @@ class App extends Component {
         <div id="main">
           <h1>Ninja's Reading List</h1>
           <BookList />
+          <AddBook />
         </div>
       </ApolloProvider>
     );
